df195df5729cfd8b8f5c45f17b887ed256547fe9
Wanderson Pinto <wanpdsantos@gmail.com>
2022-07-29 10:12:26 -0300

    included new dbt project

---
+++ dags/dbt/finances/models/example/my_first_dbt_model.sql
@@ -0,0 +1,26 @@
+/*
+    Welcome to your first dbt model!
+    Did you know that you can also configure models directly within SQL files?
+    This will override configurations stated in dbt_project.yml
+
+    Try changing "table" to "view" below
+*/
+
+{{ config(materialized='table') }}
+
+WITH source_data AS (
+
+    SELECT
+        1 AS user_id,
+        2 AS team_id
+
+)
+
+SELECT *
+FROM source_data
+
+/*
+    Uncomment the line below to remove records with null `id` values
+*/
+
+-- where id is not null
---
+++ dags/dbt/finances/models/example/my_second_dbt_model.sql
@@ -0,0 +1,5 @@
+-- Use the `ref` function to select from other models
+
+SELECT *
+FROM {{ ref('my_first_dbt_model') }}
+WHERE id = 1
